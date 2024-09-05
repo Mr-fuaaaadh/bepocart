be2645c5113a827664f6337576279b188f11ad98
Fuhad Mk <143482980+Mr-fuaaaadh@users.noreply.github.com>
2024-09-05 15:15:18 +0530

    google

--- bepocart/settings.py
+++ bepocart/settings.py
@@ -165,27 +165,27 @@ WSGI_APPLICATION = 'bepocart.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databa
 
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'bepocart',
-         'USER': 'postgres',
-         'PASSWORD': '9645848527',
-         'HOST': 'database-1.c3qgaks0kpy9.eu-north-1.rds.amazonaws.com',
-         'PORT': '5432'
-     }
- }
-
 # DATABASES = {
-#     'default':  {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'bepocart',
-#         'USER': 'postgres',
-#         'PASSWORD': '252562',
-#         'HOST': 'localhost',
-#         'PORT': '5432'    
-#     }
-# }
+#      'default': {
+#          'ENGINE': 'django.db.backends.postgresql',
+#          'NAME': 'bepocart',
+#          'USER': 'postgres',
+#          'PASSWORD': '9645848527',
+#          'HOST': 'database-1.c3qgaks0kpy9.eu-north-1.rds.amazonaws.com',
+#          'PORT': '5432'
+#      }
+#  }
+
+DATABASES = {
+    'default':  {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bepocart',
+        'USER': 'postgres',
+        'PASSWORD': '252562',
+        'HOST': 'localhost',
+        'PORT': '5432'    
+    }
+}
 
 
 
